db1c449d83d350059e2d8506fc70c741aec46b8a
shanti0x <danaciocoiu78@gmail.com>
2022-03-31 17:38:39 +0300

    Validation rules for User field

--- src/main/java/com/home/ROMS/service/user/UserRepository.java
+++ src/main/java/com/home/ROMS/service/user/UserRepository.java
@@ -3,6 +3,6 @@ package com.home.ROMS.service.user;
 import com.home.ROMS.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+interface UserRepository extends JpaRepository<User, Long> {
 
 }
